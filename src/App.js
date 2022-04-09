724bd052a8df250f708822106c8b81d9de01f9a2
Jurgen Mikk <jurgen.mikk@khk.ee>
2022-04-09 20:26:32 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PostForm from "./api/PostForm";
+
+function App() {
+
+  return (
+      <div className="app">
+         <PostForm/>
+      </div>
+  );
+}
+
+export default App;
